c92d830e55066988538933f790b5a2ad4a009e2b
MAngelesGB <mgutierrez6@ucol.mx>
2024-03-06 01:47:07 -0600

    Add pictures and give styles for it

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import './App.css'
 import {Song} from './rockSongs'
 
 let array = [
-  {title: "Wish you were here", artist: "Pink Floyd", album: "Wish you were here", listened: true},
-  {title: "Stairway to Heaven", artist: "Led Zeppelin", album: "Led Zeppelin IV", listened: false},
-  {title: "Bohemian Rhapsody", artist: "Queen", album: "A Night at the Opera", listened: true},
-  {title: "Comfortably Numb", artist: "Pink Floyd", album: "The Wall", listened: false},
-  {title: "Hotel California", artist: "Eagles", album: "Hotel California", listened: true},
+  {img:"../public/cards/wishyou.jpg", title: "Wish you were here", artist: "Pink Floyd", album: "Wish you were here", listened: true},
+  {img:"../public/cards/stairway.jpg", title: "Stairway to Heaven", artist: "Led Zeppelin", album: "Led Zeppelin IV", listened: false},
+  {img:"../public/cards/bohemia.jpg", title: "Bohemian Rhapsody", artist: "Queen", album: "A Night at the Opera", listened: true},
+  {img: "../public/cards/comfortably.jpg", title: "Comfortably Numb", artist: "Pink Floyd", album: "The Wall", listened: false},
+  {img: "../public/cards/hotelcalifornia.jpg", title:"Hotel California", artist: "Eagles", album: "Hotel California", listened: true},
 ]
 
 function App() {
-  const [ischecked, setIsChecked] = useState(false)
+  const [ischecked, setIsChecked] = useState(true)
   return (
     <>
       <header className="App-header"> 
         <h1> Rock Songs </h1>  
-        <input onClick={() => setIsChecked(!ischecked)} type="checkbox" /> Listened
+        <input onClick={() => setIsChecked(!ischecked)} type="checkbox" checked = {ischecked}/> Listened
       </header>
       {
         ischecked ? array.filter((song) => song.listened).map((song) => {
-          return <Song title={song.title} artist={song.artist} album={song.album} listened={song.listened} />
+          return <Song img={song.img} title={song.title} artist={song.artist} album={song.album} listened={song.listened} />
         }) : array.filter((song) => !song.listened).map((song) => {
-          return <Song title={song.title} artist={song.artist} album={song.album} listened={song.listened} />
+          return <Song img={song.img} title={song.title} artist={song.artist} album={song.album} listened={song.listened} />
         }) 
       }
     </>
--- src/rockSongs.jsx
+++ src/rockSongs.jsx
@@ -2,22 +2,26 @@
 export function Song(props){
     return (
         <>
-            <div className="rock-song">
-                <h2>{props.title}</h2>
-                <div className="item">
-                    <p className="attribute">Title</p><p>{props.title}</p>
+            <div className="list-song">
+                <div className="img-song">
+                    <img src={props.img} alt={props.title} />
                 </div>
-                <div className="item">
-                    <p className="attribute">Artist</p><p>{props.artist}</p>
-                </div>
-                <div className="item">
-                    <p className="attribute">Album</p><p>{props.album}</p>
-                </div>
-                <div className="item">
-                    <p className="attribute">listened</p><p>{props.listened ? "Listened" : "Not listened"}</p>
-                </div>
+                <div className="rock-song">
+                    <h2>{props.title}</h2>
+                    
+                    <div className="item">
+                        <p className="attribute">Artist</p><p>{props.artist}</p>
+                    </div>
+                    <div className="item">
+                        <p className="attribute">Album</p><p>{props.album}</p>
+                    </div>
+                    <div className="item">
+                        <p className="attribute">listened</p><p>{props.listened ? "Listened" : "Not listened"}</p>
+                    </div>
                 
+                </div>
             </div>
+            
         </>
     )
 }
